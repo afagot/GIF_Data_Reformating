a53bc58f4d8e98fbb4865330a6bf8a0298983492
afagot <alexis.fagot@ugent.be>
2017-11-13 12:52:33 +0100

    [Merge-Events] Trying to merge badly written data on scan numbers 2969 to 2971

--- include/utils.h
+++ include/utils.h
@@ -15,4 +15,13 @@ using namespace std;
 
 bool    existFile(string fName);
 
+typedef unsigned int Uint;
+
+struct RAWData {
+    int            iEvent;   //Event i
+    int            TDCNHits; //Number of hits in event i
+    vector<Uint>  *TDCCh;    //List of channels giving hits per event
+    vector<float> *TDCTS;    //List of the corresponding time stamps
+};
+
 #endif // UTILS_H
--- src/MakeDAQFile.cc
+++ src/MakeDAQFile.cc
@@ -1,4 +1,5 @@
 #include "../include/MakeDAQFile.h"
+#include "../include/utils.h"
 
 #include "TFile.h"
 #include "TTree.h"
@@ -12,25 +13,79 @@ void MakeDAQFile(string fName){
     if(existFile(fName)){
         TFile* input = new TFile(fName.c_str(),"READ");
 
-        TTree* RAWDataTree = (TTree*)input->Get("RAWData");
-        TTree* RunParameters = (TTree*)input->Get("RunParameters");
-
-        // Deactivate all branches
-        RAWDataTree->SetBranchStatus("*",0);
-
-        // Activate all the branches exept the QFlag one
-        RAWDataTree->SetBranchStatus("EventNumber*",1);
-        RAWDataTree->SetBranchStatus("number_of_hits*",1);
-        RAWDataTree->SetBranchStatus("TDC_channel*",1);
-        RAWDataTree->SetBranchStatus("TDC_TimeStamp*",1);
-
-        TFile* output = new TFile(fName.c_str(),"RECREATE");
-
-        TTree* newRAWDataTree = RAWDataTree->CloneTree();
-        TTree* newRunParameters = RunParameters->CloneTree();
+        TTree* oldRAWDataTree = (TTree*)input->Get("RAWData");
         TH1D* ID = (TH1D*)input->Get("ID");
         TH1I* Triggers = (TH1I*)input->Get("Triggers");
         TH1I* Thrs = (TH1I*)input->Get("Thrs");
+
+        TTree* oldRunParameters = (TTree*)input->Get("RunParameters");
+        TTree* newRunParameters = oldRunParameters->CloneTree();
+
+        RAWData olddata;
+
+        olddata.TDCCh = new vector<Uint>;
+        olddata.TDCTS = new vector<float>;
+        olddata.TDCCh->clear();
+        olddata.TDCTS->clear();
+
+        oldRAWDataTree->SetBranchAddress("EventNumber",    &olddata.iEvent);
+        oldRAWDataTree->SetBranchAddress("number_of_hits", &olddata.TDCNHits);
+        oldRAWDataTree->SetBranchAddress("TDC_channel",    &olddata.TDCCh);
+        oldRAWDataTree->SetBranchAddress("TDC_TimeStamp",  &olddata.TDCTS);
+
+        string newfName = "test.root";
+        TFile* output = new TFile(newfName.c_str(),"RECREATE");
+
+        TTree* newRAWDataTree = new TTree("RAWData","RAWData");
+
+        int           EventCount = -9;  //Event tag
+        int           nHits = -8;       //Number of fired TDC channels in event
+        vector<int>   TDCCh;            //List of fired TDC channels in event
+        vector<float> TDCTS;            //list of fired TDC channels time stamps
+
+        TDCCh.clear();
+        TDCTS.clear();
+
+        //Set the branches that will contain the previously defined variables
+        newRAWDataTree->Branch("EventNumber",    &EventCount, "EventNumber/I");
+        newRAWDataTree->Branch("number_of_hits", &nHits,      "number_of_hits/I");
+        newRAWDataTree->Branch("TDC_channel",    &TDCCh);
+        newRAWDataTree->Branch("TDC_TimeStamp",  &TDCTS);
+
+        Uint nEntries = oldRAWDataTree->GetEntries();
+        int lastEvent = -1;
+
+        for(Uint i=0; i<nEntries; i++){
+            oldRAWDataTree->GetEntry(i);
+
+            cout << olddata.iEvent << " " << lastEvent << endl;
+
+            if(lastEvent == -1){
+                EventCount = olddata.iEvent;
+                nHits = olddata.TDCNHits;
+                TDCCh.insert(TDCCh.end(),olddata.TDCCh->begin(),olddata.TDCCh->end());
+                TDCTS.insert(TDCTS.end(),olddata.TDCTS->begin(),olddata.TDCTS->end());
+
+                lastEvent = olddata.iEvent;
+            } else if(olddata.iEvent == lastEvent){
+                EventCount = olddata.iEvent;
+                nHits = nHits + olddata.TDCNHits;
+                TDCCh.insert(TDCCh.end(),olddata.TDCCh->begin(),olddata.TDCCh->end());
+                TDCTS.insert(TDCTS.end(),olddata.TDCTS->begin(),olddata.TDCTS->end());
+            } else {
+                newRAWDataTree->Fill();
+
+                EventCount = olddata.iEvent;
+                nHits = olddata.TDCNHits;
+                TDCCh.clear();
+                TDCTS.clear();
+                TDCCh.insert(TDCCh.end(),olddata.TDCCh->begin(),olddata.TDCCh->end());
+                TDCTS.insert(TDCTS.end(),olddata.TDCTS->begin(),olddata.TDCTS->end());
+
+                lastEvent = olddata.iEvent;
+            }
+        }
+        newRAWDataTree->Fill();
 
         newRAWDataTree->Write();
         newRunParameters->Write();
